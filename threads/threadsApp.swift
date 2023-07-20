1dbdf04de4dbafb8e7df45d1f29bedc0515edd1d
David Daly <ddalyjr@gmail.com>
2023-07-19 20:17:35 -0400

    Initial Commit

---
+++ threads/threadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  threadsApp.swift
+//  threads
+//
+//  Created by David Daly on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct threadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
